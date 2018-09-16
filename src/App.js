5aec5bdab729e10791840e2496d52e77e04006e7
Oliver Lai <oliverlai55@gmail.com>
2018-09-16 19:09:48 -0400

    Add more pretty componenets

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import { Elements, StripeProvider } from 'react-stripe-elements';
 import CheckoutForm from './CheckoutForm';
 
 class App extends Component {
+  constructor() {
+    super();
+    // this.state = {
+    //   elementFontSize: window.innerWidth < 450 ? '14px' : '18px',
+    // };
+    // window.addEventListener('resize', () => {
+    //   if (window.innerWidth < 450 && this.state.elementFontSize !== '14px') {
+    //     this.setState({ elementFontSize: '14px' });
+    //   } else if (
+    //     window.innerWidth >= 450 &&
+    //     this.state.elementFontSize !== '18px'
+    //   ) {
+    //     this.setState({ elementFontSize: '18px' });
+    //   }
+    // });
+  }
   render() {
+    // const { elementFontSize } = this.state;
     return (
       <StripeProvider apiKey="pk_test_u4xe0pnznOmQyQ86eBu0tqzV">
-        <div className="example">
-          <h1>React Stripe Elements Example</h1>
+        <div className="Checkout">
+          <h1>Stripe Checkout Example</h1>
           <Elements>
             <CheckoutForm />
           </Elements>
--- src/CheckoutForm.js
+++ src/CheckoutForm.js
@@ -1,34 +1,133 @@
 import React, { Component } from 'react';
-import { CardElement, injectStripe } from 'react-stripe-elements';
+import { CardElement, injectStripe, CardNumberElement, CardExpiryElement, CardCVCElement, PostalCodeElement } from 'react-stripe-elements';
+
+const handleBlur = () => {
+  console.log('[blur]');
+};
+const handleChange = (change) => {
+  console.log('[change]', change);
+};
+const handleClick = () => {
+  console.log('[click]');
+};
+const handleFocus = () => {
+  console.log('[focus]');
+};
+const handleReady = () => {
+  console.log('[ready]');
+};
+
+const createOptions = (fontSize, padding) => {
+  return {
+    style: {
+      base: {
+        fontSize,
+        color: '#424770',
+        letterSpacing: '0.025em',
+        fontFamily: 'Source Code Pro, monospace', '::placeholder': {
+          color: '#aab7c4',
+        },
+        padding,
+      },
+      invalid: {
+        color: '#9e2146',
+      },
+    },
+  };
+};
 
 class CheckoutForm extends Component {
   constructor(props) {
     super(props);
+
     this.state = { complete: false };
-    this.submit = this.submit.bind(this)
+    // console.log('props', this.props);
+    // this.submit = this.submit.bind(this)
+    // this.handleSubmit = this.props.stripe.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   async submit(ev) {
     // User Clicked submit`
-    let { token } = await this.props.stripe.createToken({ name: 'Name'});
+    let { token } = await this.props.stripe.createToken({ name: 'Name' });
     let response = await fetch('/charge', {
       method: 'POST',
-      headers: { "Content-Type": "text/plain"},
+      headers: { "Content-Type": "text/plain" },
       body: token.id
     });
 
-    if (response.ok) this.setState({ complete: true});
+    if (response.ok) this.setState({ complete: true });
+  }
+
+  async handleSubmit(ev) {
+    ev.preventDefault();
+    let { token } = await this.props.stripe.createToken({
+      name: 'Name'
+    });
+    let response = await fetch('/charge', {
+      method: 'POST',
+      headers: { "Content-Type": "text/plain" },
+      body: token.id
+    });
+
+    if (response.ok) this.setState({ complete: true });
+
   }
 
   render() {
     if (this.state.complete) return <h1>Purchase Complete</h1>
 
     return (
-      <div className="checkout">
-        <p>Would you like to complete the purchase?</p>
-        <CardElement />
-        <button onClick={this.submit}>Send</button>
-      </div>
+      // <div className="checkout">
+      //   <p>Would you like to complete the purchase?</p>
+      //   <CardElement />
+      //   <button onClick={this.submit}>Send</button>
+      // </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Card number
+          <CardNumberElement
+            onBlur={handleBlur}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onReady={handleReady}
+            {...createOptions(18)}
+          />
+        </label>
+
+        <label>
+          Expiration date
+          <CardExpiryElement
+            onBlur={handleBlur}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onReady={handleReady}
+            {...createOptions(18)}
+          />
+        </label>
+
+        <label>
+          CVC
+          <CardCVCElement
+            onBlur={handleBlur}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onReady={handleReady}
+            {...createOptions(18)}
+          />
+        </label>
+        <label>
+          Postal code
+          <PostalCodeElement
+            onBlur={handleBlur}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onReady={handleReady}
+            {...createOptions(18)}
+          />
+        </label>
+        <button>Pay</button>
+      </form>
     );
   }
 }
