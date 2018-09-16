f79b6de50ee581ac05a535d2d4e05509c6399d24
Oliver Lai <oliverlai55@gmail.com>
2018-09-16 00:51:52 -0400

    Init commit

---
+++ src/CheckoutForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { CardElement, injectStripe } from 'react-stripe-elements';
+
+class CheckoutForm extends Component {
+  constructor(props) {
+    super(props);
+    this.submit = this.submit.bind(this)
+  }
+
+  async submit(ev) {
+    // User Clicked submit
+  }
+}
